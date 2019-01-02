766ea05fba896f9e72c44d688037c592f1ebe3d1
Trent Moss <trentmoss05@gmail.com>
2019-01-02 15:45:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
-def reverse_each_word(string)
-  sentence.split.collect do |word|
-    word.reverse.join(" ")
-end
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
